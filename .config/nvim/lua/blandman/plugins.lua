7a79507c4335e83b60de5a46755824c49319ccdd
Stephen <stebland1@gmail.com>
2025-05-24 13:58:04 +0100

    Migrate to lazy

--- .config/nvim/lua/blandman/plugins.lua
+++ .config/nvim/lua/blandman/plugins.lua
@@ -1,97 +1,18 @@
-local status, packer = pcall(require, "packer")
-if not status then
-	print("Packer is not installed")
-	return
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 
-vim.cmd([[packadd packer.nvim]])
+vim.opt.rtp:prepend(lazypath)
 
-packer.startup(function(use)
-	use("wbthomason/packer.nvim")
-	use("nvim-lualine/lualine.nvim") -- Statusline
-	use("nvim-lua/plenary.nvim") -- Common utilities
-	use("onsails/lspkind-nvim") -- vscode-like pictograms
-	use("wbthomason/lsp-status.nvim")
-	use("rose-pine/neovim") -- Colorscheme
-
-	-- LSP
-	use({
-		"neovim/nvim-lspconfig",
-		requires = {
-			-- LSP Support
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
-			{ "jose-elias-alvarez/null-ls.nvim" },
-
-			-- Formatting
-			{ "stevearc/conform.nvim" },
-
-			-- Linting
-			{ "mfussenegger/nvim-lint" },
-
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
-
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-	})
-
-	-- Debugger
-	use({
-		"mfussenegger/nvim-dap",
-		requires = {
-			"mfussenegger/nvim-dap-python",
-			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
-			"williamboman/mason.nvim",
-		},
-	})
-
-	use("folke/trouble.nvim")
-
-	use({ "j-hui/fidget.nvim", after = "nvim-lspconfig" })
-
-	use("tpope/vim-surround")
-	use("tpope/vim-rhubarb")
-	use("tpope/vim-fugitive")
-	use("tpope/vim-unimpaired")
-
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		requires = {
-			"nvim-treesitter/nvim-treesitter-context",
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
-	})
-
-	use("kyazdani42/nvim-web-devicons") -- File icons
-
-	-- Telescope
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = {
-			{ "nvim-telescope/telescope-file-browser.nvim" },
-			{ "nvim-telescope/telescope-fzf-native.nvim",  run = "make" }
-		}
-	})
-
-	use("windwp/nvim-autopairs")
-	use("norcalli/nvim-colorizer.lua")
-
-	-- use("github/copilot.vim")
-	use("lewis6991/gitsigns.nvim")
-	use("theprimeagen/harpoon")
-	use("numToStr/FTerm.nvim")
-	use("tpope/vim-commentary")
-	use("mbbill/undotree")
-	use("christoomey/vim-tmux-navigator")
-	use("epwalsh/obsidian.nvim") -- Notes
-end)
+require("lazy").setup("plugins")
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,103 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim", -- Statusline
+		"nvim-lua/plenary.nvim", -- Common utilities
+		"onsails/lspkind-nvim", -- vscode-like pictograms
+		"wbthomason/lsp-status.nvim",
+		"rose-pine/neovim", -- Colorscheme
+		-- "catppuccin/nvim",
+
+		-- LSP
+		{
+			"neovim/nvim-lspconfig",
+			dependencies = {
+				-- LSP Support
+				{ "williamboman/mason.nvim" },
+				{ "williamboman/mason-lspconfig.nvim" },
+				{ "jose-elias-alvarez/null-ls.nvim" },
+
+				-- Formatting
+				{ "stevearc/conform.nvim" },
+
+				-- Linting
+				{ "mfussenegger/nvim-lint" },
+
+				-- Autocompletion
+				{ "hrsh7th/nvim-cmp" },
+				{ "hrsh7th/cmp-buffer" },
+				{ "hrsh7th/cmp-path" },
+				{ "saadparwaiz1/cmp_luasnip" },
+				{ "hrsh7th/cmp-nvim-lsp" },
+				{ "hrsh7th/cmp-nvim-lua" },
+
+				-- Snippets
+				{ "L3MON4D3/LuaSnip" },
+				{ "rafamadriz/friendly-snippets" },
+
+				{ "j-hui/fidget.nvim" },
+			},
+		},
+
+		-- Debugger
+		{
+			"mfussenegger/nvim-dap",
+			dependencies = {
+				"mfussenegger/nvim-dap-python",
+				"rcarriga/nvim-dap-ui",
+				"theHamsta/nvim-dap-virtual-text",
+				"nvim-neotest/nvim-nio",
+				"williamboman/mason.nvim",
+			},
+		},
+
+		-- Testing
+		{
+			"nvim-neotest/neotest",
+			dependencies = {
+				"nvim-neotest/nvim-nio",
+				"nvim-lua/plenary.nvim",
+				"antoinemadec/FixCursorHold.nvim",
+				"nvim-treesitter/nvim-treesitter",
+				"nvim-neotest/neotest-python",
+			},
+		},
+
+		"folke/trouble.nvim",
+		"tpope/vim-surround",
+		"tpope/vim-rhubarb",
+		"tpope/vim-fugitive",
+		"tpope/vim-unimpaired",
+		"tpope/vim-commentary",
+
+		{
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate",
+			dependencies = {
+				"nvim-treesitter/nvim-treesitter-context",
+				"JoosepAlviste/nvim-ts-context-commentstring",
+			},
+		},
+
+		"kyazdani42/nvim-web-devicons", -- File icons
+
+		-- Telescope
+		{
+			"nvim-telescope/telescope.nvim",
+			dependencies = {
+				{ "nvim-telescope/telescope-file-browser.nvim" },
+				{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+			},
+		},
+
+		"windwp/nvim-autopairs",
+		"norcalli/nvim-colorizer.lua",
+
+		-- "github/copilot.vim",
+		"lewis6991/gitsigns.nvim",
+		"theprimeagen/harpoon",
+		"numToStr/FTerm.nvim",
+		"mbbill/undotree",
+		"christoomey/vim-tmux-navigator",
+		"epwalsh/obsidian.nvim", -- Notes
+	},
+}
