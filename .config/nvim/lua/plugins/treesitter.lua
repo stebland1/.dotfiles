4284887a5ca2b773ca1862968669ecfd94caf013
Stephen Bland <stebland1@gmail.com>
2025-06-30 10:53:54 +0100

    Ensure treesitter fires up

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,35 +6,37 @@ return {
 		"nvim-treesitter/nvim-treesitter-context",
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
-	opts = {
-		highlight = {
-			enable = true,
-			disable = {},
-		},
-		indent = {
-			enable = true,
-			disable = {},
-		},
-		ensure_installed = {
-			"javascript",
-			"tsx",
-			"toml",
-			"fish",
-			"php",
-			"json",
-			"yaml",
-			"swift",
-			"css",
-			"html",
-			"lua",
-			"jsdoc",
-			"python",
-			"markdown",
-			"markdown_inline",
-		},
-		autotag = {
-			enable = true,
-			enable_close_on_slash = false,
-		},
-	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			highlight = {
+				enable = true,
+				disable = {},
+			},
+			indent = {
+				enable = true,
+				disable = {},
+			},
+			ensure_installed = {
+				"javascript",
+				"tsx",
+				"toml",
+				"fish",
+				"php",
+				"json",
+				"yaml",
+				"swift",
+				"css",
+				"html",
+				"lua",
+				"jsdoc",
+				"python",
+				"markdown",
+				"markdown_inline",
+			},
+			autotag = {
+				enable = true,
+				enable_close_on_slash = false,
+			},
+		})
+	end,
 }
